f275f26f9aae65eb00fa3350bc060a6b4941b2a0
Bappi1234 <harunbappi1234@gmail.com>
2024-09-20 19:05:11 +0600

    cashout added

--- js/cashout.js
+++ js/cashout.js
@@ -1,1 +1,16 @@
-document.getElementById("btn-cash-out");
+document.getElementById("btn-cash-out").addEventListener("click", function (e) {
+  e.preventDefault();
+  const cashOut = getInputfieldValue("cash-out-input");
+  const pinNumber = getInputfieldValue("cash-out-pin");
+  if (pinNumber != 1234) {
+    alert("wWrong input! Please try again.");
+    return;
+  }
+  const balance = getInputTextValue("available-balance");
+  const availableBalance = balance - cashOut;
+  if (cashOut > balance || cashOut < 0) {
+    alert("Wrong input! Please try again.");
+    return;
+  }
+  document.getElementById("available-balance").innerText = availableBalance;
+});
